89f0e04019150d413b6c269604086f1b7630d1d3
NAVANEESHKM <navaneeshmuthusamy@gmail.com>
2023-08-28 18:08:19 +0530

    u

--- client.go
+++ client.go
@@ -20,24 +20,29 @@ func main(){
 
 	//Add a task
 
-	task:=&pb.Task{
+	customer:=&pb.Customer{
 		Title:"Buy groceries",
+		Customer_ID:23,
+		Customer_Name:"sachin",
+		Account_ID:786,
+        Balance:46756,
+		Bank_ID:98,
 	}
-	addResp,err:=client.AddTask(context.Background(),task)
+	addResp,err:=client.AddTask(context.Background(),customer)
 
 	if err!=nil{
 		log.Fatalf("Failed to add tasks: %v",err)
 	}
-	fmt.Printf("Added task with ID: %s\n",addResp.Id)
+	fmt.Printf("Added task with ID: %s\n",addResp.Title)
 
 	
 	// response,err:=client.AddTask(context.Background(),&pb.HelloRequest{Name:name})
-	tasksResp,err:=client.GetTasks(context.Background(),&pb.Empty{})
-	if err!=nil{
-		log.Fatalf("Failed to call SayHello: %v",err)
-	}
-	fmt.Println("Tasks:")
-	for _,task:=range tasksResp.Tasks{
-          fmt.Printf("ID: %s,Title:%s,completed:%v\n",task.Id,task.Title,task.Completed)
-	}
+	// tasksResp,err:=client.GetTasks(context.Background(),&pb.Empty{})
+	// if err!=nil{
+	// 	log.Fatalf("Failed to call SayHello: %v",err)
+	// }
+	// fmt.Println("Tasks:")
+	// for _,task:=range tasksResp.Tasks{
+    //       fmt.Printf("ID: %s,Title:%s,completed:%v\n",task.Id,task.Title,task.Completed)
+	// }
 }
